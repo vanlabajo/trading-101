939665131deb918768dc9abf97474bee6988bf2b
Van Labajo <van.labajo@serialmultivision.com>
2021-02-01 13:58:31 +0800

    add integration tests

---
+++ Trading101/IntegrationTests/AvMarketDataTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TradingStrategies.Wrappers;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class AvMarketDataTests
+    {
+        private string avApiKey;
+        private ServiceProvider serviceProvider;
+
+        public AvMarketDataTests()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<AvMarketDataTests>()
+                .Build();
+
+            var services = new ServiceCollection();
+            services.AddHttpClient();
+
+            serviceProvider = services.BuildServiceProvider();
+
+            avApiKey = configuration["AvApiKey"];
+        }
+
+        [Fact]
+        public async Task GetQuoteAsync()
+        {
+            var factory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            var marketData = new AvMarketData(avApiKey, factory);
+
+            var quote = await marketData.GetQuoteAsync("MSFT");
+
+            Assert.NotNull(quote);
+        }
+
+        [Fact]
+        public async Task GetLatestEmaAsync()
+        {
+            var factory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            var marketData = new AvMarketData(avApiKey, factory);
+
+            var ema9 = await marketData.GetLatestEmaAsync("MSFT", 9);
+
+            Assert.True(ema9 > 0);
+        }
+
+        [Fact]
+        public async Task GetLatestSmaAsync()
+        {
+            var factory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            var marketData = new AvMarketData(avApiKey, factory);
+
+            var sma9 = await marketData.GetLatestSmaAsync("MSFT", 9);
+
+            Assert.True(sma9 > 0);
+        }
+
+        [Fact]
+        public async Task GetIntradayHighestHighLowestLowAsync()
+        {
+            var factory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            var marketData = new AvMarketData(avApiKey, factory);
+
+            var (HighestHigh, LowestLow) = await marketData.GetIntradayHighestHighLowestLowAsync("MSFT");
+
+            Assert.True(HighestHigh != 0);
+            Assert.True(LowestLow != 0);
+            Assert.True(HighestHigh > LowestLow);
+        }
+    }
+}
---
+++ Trading101/IntegrationTests/TwsBrokerTests.cs
@@ -0,0 +1,63 @@
+﻿using AutoFinance.Broker.InteractiveBrokers;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+using TradingStrategies.Constants;
+using TradingStrategies.Wrappers;
+using Xunit;
+
+namespace IntegrationTests
+{
+    /// <summary>
+    /// These tests require that TWS be running on the local machine.
+    /// They also require that the API is enabled, and that 127.0.0.1 and 0:0:0:0:0:0:0:1 are in the Allowed Hosts section of the TWS configuration.
+    /// The setting can be found in the TWS window under "Account -> Global Settings -> API".
+    /// </summary>
+    public class TwsBrokerTests
+    {
+        private string twsAccountId;
+
+        public TwsBrokerTests()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<TwsBrokerTests>()
+                .Build();
+
+            twsAccountId = configuration["TwsAccountId"];
+        }
+
+        [Fact]
+        public async Task GetAvailableFundsAsync()
+        {
+            var twsObjectFactory = new TwsObjectFactory("localhost", 7497, 1);
+            var broker = new TwsBroker(twsAccountId, twsObjectFactory);
+
+            var availableFunds = await broker.GetAvailableFundsAsync(Currency.USD);
+
+            Assert.True(availableFunds > 0);
+
+            await twsObjectFactory.TwsController.DisconnectAsync();
+        }
+
+        [Fact]
+        public async Task PlaceBracketOrderAsync()
+        {
+            var twsObjectFactory = new TwsObjectFactory("localhost", 7497, 1);
+            var broker = new TwsBroker(twsAccountId, twsObjectFactory);
+
+            var entryOrderId = await broker.PlaceBracketOrderAsync("MSFT", "BUY", 1m, 238.93m, 238.73m, 239.33m);
+            Assert.True(entryOrderId > 0);
+
+            Thread.Sleep(1000); // TWS takes some time to put the order in the portfolio. Wait for it.
+
+            var openOrders = await broker.GetOpenOrderIdsAsync("MSFT");
+
+            Assert.True(openOrders.Count == 3);
+
+            var result = await broker.CancelOrderAsync(entryOrderId);
+            Assert.True(result);
+
+            await twsObjectFactory.TwsController.DisconnectAsync();
+        }
+    }
+}
